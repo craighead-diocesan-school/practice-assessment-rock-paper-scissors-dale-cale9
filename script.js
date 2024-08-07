8f91b25cc5b5953f4e72b7a32c899e438609944a
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-08-07 14:35:02 +1200

    Testing
    It is asking the question and also replies back with rock, paper or scissors and also outputs win or lose but still in process.

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 let answers = [
-'Paper',
-'scissors',
-'Rock'
+    'Paper',
+    'scissors',
+    'Rock'
 ]
+
 const randomAnswer = 3
 function Userinput() {
+
     // Asks the user to play Paper, Scissors, Rock and then asks then to insert something to start the game. 
-    let userChoice = prompt ("Let's play Paper, Scissors, Rock! Insert what you want to say")
-    let index = Math.floor(Math.random()* randomAnswer)
+    let userChoice = prompt("Let's play Paper, Scissors, Rock! Insert what you want to say")
+    let index = Math.floor(Math.random() * randomAnswer)
+    let computerAnswer = answers[index]
     // Outputs a reply back to the user with a random word in the array. 
-    alert (answers [index] +'!')
-}
+    alert(computerAnswer + '!')
 
 
-    if( userChoice == 'paper' && answers [index] == 'Rock' ) {
-        alert('Round 2')
+
+    if (userChoice == computerAnswer) {
+        alert('Tied')
     }
+    else if (
+        userChoice == 'rock' && computerAnswer == 'Paper' ||
+        userChoice == 'paper' && computerAnswer == 'Scissors' ||
+        userChoice == 'scissors' && computerAnswer == 'Rock') {
+        alert('You lose womp womp.  ')
+    }
+    else if (
+        userChoice == 'paper' && computerAnswer == 'Rock' ||
+        userChoice == 'sciccors' && computerAnswer == 'paper' ||
+        userChoice == 'rock' && computerAnswer == 'Sciccors') {
+        alert('YAY, you win.')
+    }
+
+
+} 
